3b1f1b051183236eb37f7a240a3fdbb81f38112d
William <69988679+Killaship@users.noreply.github.com>
2023-05-16 13:53:13 -0400

    oops

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import socket
 class TCP:
     host = '127.0.0.1' # TODO: Replace host and port values with those from a config file
     port = 80
-    def TCPstart(self):
+    def TCPstart():
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind(self.host, self.port)
+        sock.bind(host, port)
         s.listen(5) # normal max is 5 users queued
         print("Listening on ", s.getsockname())
         while True:
