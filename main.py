649260de106260fc9ed125a422ef76fc2bf68d9a
William <69988679+Killaship@users.noreply.github.com>
2023-05-16 14:04:52 -0400

    redo

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import socket
 
-class TCP:
-    host = '127.0.0.1' # TODO: Replace host and port values with those from a config file
-    port = 80
-    def TCPstart():
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind(TCP.host, TCP.port)
-        s.listen(5) # normal max is 5 users queued
-        print("Listening on ", s.getsockname())
-        while True:
-            user = s.accept()
-            print(user, " connected")
-            data = user.recv(4096) # read first 4K of data
-            user.sendall(data) # echo everything back
+host = "127.0.0.1"
+port = 80
 
-            
-if __name__ == '__main__':
-    TCP.TCPstart()
-
+def TCPstart():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(host, port)
+    sock.listen(5) # normal max is 5 users queued
+    print("Listening on ", sock.getsockname())
+    while True:
+        user = sock.accept()
+        print(user, " connected")
+        data = user.recv(4096) # read first 4K of data
+        user.sendall(data) # echo everything back
