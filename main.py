763acd3487afbaaf758ecf6b50ebc8e31899e74b
William <69988679+Killaship@users.noreply.github.com>
2023-05-16 13:51:41 -0400

    forgot "start" arg

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import socket
 class TCP:
     host = '127.0.0.1' # TODO: Replace host and port values with those from a config file
     port = 80
-    def TCPstart():
+    def TCPstart(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind(self.host, self.port)
         s.listen(5) # normal max is 5 users queued
