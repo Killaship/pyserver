d1981bb54e92a087de3c9becf3a1841c5be3e5e2
William <69988679+Killaship@users.noreply.github.com>
2023-05-17 13:35:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ import socket
 import time
 import sys
 
+def contprompt():
+    prompt = input("Continue? [y/N]").lower()
+    if(prompt == "n"):
+        raise SystemExit(1)
+        return 1
+    elif(prompt == "y"):
+        return 0
+    else:
+        print("Invalid response!")
+        return 1
 
 current_module = sys.modules[__name__]
 try:
     logfile = open(f'logs/std.log-{str(time.time()).strip(" ")}', 'w')
 except Exception as e:
     print("Error opening log file!\n\n", e)
-    raise SystemExit(1)
+    while contprompt():
+        pass
+    return
 
 def log(input):
-    logfile.write(f"\n[{str(time.time().strip(' '))}]\n{str(input)}\n")
+    logfile.write(f"\n[{str(time.time()).strip(' ')}]\n{str(input)}\n")
     
 def cleanup():
     logfile.close()
