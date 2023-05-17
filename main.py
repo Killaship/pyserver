b1977a9bdbd737b2fe79a37d89477e3f8020e8a8
William <69988679+Killaship@users.noreply.github.com>
2023-05-17 08:11:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def handler(data):
     request = Request(data)
     
     try:
-        methodhandler = getattr(self, 'handler_%s' % request.method) # useful hack I found
+        methodhandler = getattr('handler_%s' % request.method) # useful hack I found
     except AttributeError:
         handler = handler_501
     response = methodhandler(request)
