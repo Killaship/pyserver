a9d1126c5e9698b0088f904266de062cbb7ae589
William <69988679+Killaship@users.noreply.github.com>
2023-05-17 22:00:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def handler_GET(request):
     if(os.path.exists(loc)):
         response = b"HTTP/1.1 200 OK\r\n"
         with open(loc, 'r') as file:
-            body = bytes(file.read(), 'utf-8')
+            body = file.read()
             type = mimetypes.guess_type(file)[0] or 'text/html'
         
     else:
