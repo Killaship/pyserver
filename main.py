58b1d8d87ed36b3aa8d1a02728d956950e4121d7
William <69988679+Killaship@users.noreply.github.com>
2023-06-21 21:40:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ def handler_GET(request):
     except Exception as e:
         if(verbosity >= 1):
             print("error in sending GET request content\n", e)
-            log(str("error in sending GET request content\n", e))
+            log(str("error in sending GET request content\n", str(e)))
     return b"".join([response, header, bline, body])
 
 def handler_501(request):
