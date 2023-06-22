aa437f5d4d021a3d0c5338b31f97f22fa159d12e
William <69988679+Killaship@users.noreply.github.com>
2023-06-21 20:40:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ class Request:
             self.httpver = chunks[2].decode()
         self.path = self.uri.split("?", 1)[0].strip('/')
 
-        self.params = {} #dict([p.split('=') for p in parsed_path[4].split('&')])
+        self.params = "" #dict([p.split('=') for p in parsed_path[4].split('&')])
         print(str(self.method + "\n" + self.uri + "\n" + self.http_version + "\n" + self.path + "\n" + self.params))
         logfile.write(str(self.method + "\n" + self.uri + "\n" + self.http_version + "\n" + self.path + "\n" + self.params))
 
