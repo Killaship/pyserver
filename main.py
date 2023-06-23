58a49a45b0f862ff9e2e2175a9c74b0707eda66b
William <69988679+Killaship@users.noreply.github.com>
2023-06-22 23:12:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -170,13 +170,10 @@ def handler_GET(request):
             print("response:\n"+str(response+header+bline+body[0:maxreplog])+"[rest truncated for size]\n")
     elif(verbosity == 1):
         print("responded")    
-    try:
-        return b"".join([response, header, bytes(str("<br><p>"+str(request.params.keys()[0])+"</p><br>)<br>:)"), "utf-8"), bline, body])
-    except Exception as e:
-        if(verbosity >= 1):
-            print("error in sending GET request content\n", repr(e))
-            logfile.write("error in sending GET request content\n" + repr(e))
-        return b"".join([response, header, bline, body])
+
+    
+    return b"".join([response, header, bline, body])
+        
 
 def handler_501(request):
     response = b"HTTP/1.1 501 Not Implemented\r\n"
